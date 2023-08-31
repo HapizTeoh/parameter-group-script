70179d055851bae9aed766b459f4aa7c742c2e2d
HapizTeoh <hapizteoh99@gmail.com>
2023-08-31 13:20:02 +0800

    Pylint score update to 8.71

--- parameter_group.py
+++ parameter_group.py
@@ -1,8 +1,8 @@
 import os
+import logging
+import csv
 from urllib import response
 import boto3
-import pprint, logging
-import csv
 from botocore.exceptions import ClientError
 from dotenv import load_dotenv
 
@@ -16,39 +16,38 @@ source_parameter_group = os.getenv("source_file")
 target_parameter_group = os.getenv("target_parameter_group")
 parameter_group_family = os.getenv("parameter_group_family")
 access_profile = os.getenv("aws_profile")
-found_flag = bool
+found_flag = False
 
 def init_aws_session():
+    """ Create a new AWS session"""
     return boto3.setup_default_session(profile_name=f'{access_profile}')
 
-# Get all the parameter groups
 def rds_get_parameter_group_list():
+    """Get all parameter groups"""
     rds = boto3.client('rds')
-    response = rds.describe_db_parameter_groups()
-    return response["DBParameterGroups"]
+    responses = rds.describe_db_parameter_groups()
+    return responses["DBParameterGroups"]
 
 def rds_update_parameters(parameter_group_name, update_parameters):
-        rds = boto3.client('rds')
-        """
-        Updates parameters in a custom DB parameter group.
+    """
+    Updates parameters in a custom DB parameter group.
 
-        :param parameter_group_name: The name of the parameter group to update.
-        :param update_parameters: The parameters to update in the group.
-        :return: Data about the modified parameter group.
-        """
-        try:
-            response = rds.modify_db_parameter_group(
-                DBParameterGroupName=parameter_group_name, Parameters=update_parameters)
-        except ClientError as err:
-            logging.error(
-                "Couldn't update parameters in %s. Here's why: %s: %s", parameter_group_name,
-                err.response['Error']['Code'], err.response['Error']['Message'])
-            raise
-        else:
-            return response
+    :param parameter_group_name: The name of the parameter group to update.
+    :param update_parameters: The parameters to update in the group.
+    :return: Data about the modified parameter group.
+    """
+    rds = boto3.client('rds')
+    try:
+        responses = rds.modify_db_parameter_group(
+            DBParameterGroupName=parameter_group_name, Parameters=update_parameters)
+        return responses
+    except ClientError as err:
+        logging.error(
+            "Couldn't update parameters in %s. Here's why: %s: %s", parameter_group_name,
+            err.response['Error']['Code'], err.response['Error']['Message'])
+        raise
 
 def get_parameters(parameter_group_name, name_prefix='', source=None):
-    rds = boto3.client('rds')
     """
     Gets the parameters that are contained in a DB parameter group.
 
@@ -60,6 +59,7 @@ def get_parameters(parameter_group_name, name_prefix='', source=None):
                     were set by a user.
     :return: The list of requested parameters.
     """
+    rds = boto3.client('rds')
     try:
         kwargs = {'DBParameterGroupName': parameter_group_name}
         if source is not None:
@@ -69,61 +69,64 @@ def get_parameters(parameter_group_name, name_prefix='', source=None):
         for page in paginator.paginate(**kwargs):
             parameters += [
                 p for p in page['Parameters'] if p['ParameterName'].startswith(name_prefix)]
+        return parameters
     except ClientError as err:
         logging.error(
             "Couldn't get parameters for %s. Here's why: %s: %s", parameter_group_name,
             err.response['Error']['Code'], err.response['Error']['Message'])
         raise
-    else:
-        return parameters
 
 def read_csv(file_path):
-    logging.info("Reading CSV file: "+file_path)
-    with open(file_path, mode='r') as csv_file:
+    """Read CSV file and create a dictionary"""
+    logging.info("Reading CSV file: %s", file_path)
+    with open(file_path, mode='r', encoding="utf-8") as csv_file:
         csv_reader = csv.DictReader(csv_file)
         line_count = 0
         try:
             for row in csv_reader:
-                    if line_count == 0:
-                        logging.info(f'Column names are {", ".join(row)}')
-                        line_count += 1
-                    if row["Name"] != "":
-                        data.update({row["Name"] : row["Value"]})
+                if line_count == 0:
+                    logging.info(f'Column names are {", ".join(row)}')
                     line_count += 1
-            logging.info(f'Processed {line_count} rows including header.')
+                if row["Name"] != "":
+                    data.update({row["Name"] : row["Value"]})
+                line_count += 1
+            logging.info('Processed %s rows including header.', line_count)
         except KeyError:
             logging.info("Column names are not correct!")
-        
+
 if __name__ == "__main__":
     read_csv(f'{source_parameter_group}')
     for key,value in data.items():
-        logging.info(f'Parameter_name:{key}," values: "{value}')
+        logging.info('Parameter_name: %s," values: "%s"', key, value)
         sheet_list.update( {key : value} )
-    
+
     session = init_aws_session()
     pg_groups = rds_get_parameter_group_list()
     #loop all parameter groups
-    for x in pg_groups:                                                
+    for x in pg_groups:
         #print parameter groups that is in postgres family
         if parameter_group_family in x["DBParameterGroupFamily"]:
             if target_parameter_group in x["DBParameterGroupName"]:
-                logging.info("Target parameter group found: "+x["DBParameterGroupName"])
+                logging.info("Target parameter group found: %s",x["DBParameterGroupName"])
                 found_flag = True
                 #print the parameter group parameters
                 parameters = get_parameters(x["DBParameterGroupName"])
                 #print the parameters and values
-                for  i in parameters:
-                        #Find the matching parameter group in source csv
-                        if i["ParameterName"] in sheet_list.keys():
-                            parameters_name = i["ParameterName"]
-                            parameters_apply_type = i["ApplyType"]
-                            for n in sheet_list:
-                                if parameters_name == n:     
-                                    print("\nParameter name from pg10: "+n,"\nParameter name from pg11: "+parameters_name,"\nValue to copy: "+sheet_list[n])
-                                    if parameters_apply_type == "static":
-                                        Parameters_args=[{'ParameterName': parameters_name,'ParameterValue': sheet_list[n],'ApplyMethod': 'pending-reboot' }]
-                                    else:
-                                        Parameters_args=[{'ParameterName': parameters_name,'ParameterValue': sheet_list[n],'ApplyMethod': 'immediate' }]
-                                    #rds_update_parameters(target_parameter_group,Parameters_args)
-    if found_flag != True:
-        logging.info("Target parameter group not found!")
+                for i in parameters:
+                    #Find the matching parameter group in source csv
+                    if i["ParameterName"] in sheet_list:
+                        parameters_name = i["ParameterName"]
+                        parameters_apply_type = i["ApplyType"]
+                        for key,value in sheet_list.items():
+                            if parameters_name == key:
+                                print("\nParameter name from pg10: "+key,"\nParameter name from pg11: "
+                                      +parameters_name,"\nValue to copy: "+value)
+                                if parameters_apply_type == "static":
+                                    Parameters_args=[{'ParameterName': parameters_name,
+                                                      'ParameterValue': value,'ApplyMethod': 'pending-reboot' }]
+                                else:
+                                    Parameters_args=[{'ParameterName': parameters_name,
+                                                      'ParameterValue': value,'ApplyMethod': 'immediate' }]
+                                #rds_update_parameters(target_parameter_group,Parameters_args)
+    if found_flag is not True:
+        logging.info("Target parameter group not found!\n")
